7374818eb043b4ffc003325ea5b0370b7f3f92e4
Marcos Olavo Silva Buganeme <marcos.after@gmail.com>
2020-08-24 14:56:14 -0300

    corrigindo problema de rota

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express'
 
 import DefaultRouter from './domain/default.router'
-import PagamentoRoute from './domain/filiais/filial.router'
+import FilialRoute from './domain/filiais/filial.router'
 
 const Routes = Router()
 
 Routes
   .use('/', DefaultRouter)
-  .use('/pagamentos', PagamentoRoute)
+  .use('/filiais', FilialRoute)
 
 export default Routes
