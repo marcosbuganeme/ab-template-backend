bb78dfa9aca0101406837d997bd37783de3afd2e
Marcos Olavo Silva Buganeme <marcos.after@gmail.com>
2020-09-01 08:51:04 -0300

    commit sql de buscar cliente

---
+++ src/domain/clientes/cliente.sql.ts
@@ -0,0 +1,22 @@
+class ClienteSql {
+
+  porFiltro = (codigo: number, nome: string) => {
+    return `
+      SELECT 
+        C.CODCLI, 
+        C.CLIENTE,
+        C.ENDERCOB,
+        C.BAIRROCOB,
+        C.FANTASIA,
+        C.DTCADASTRO
+      FROM 
+        PCCLIENT C
+      WHERE
+        C.CODCLI = ${codigo}
+      OR
+        UPPER(C.CLIENTE) LIKE UPPER('%${nome}%')
+    `
+  }
+}
+
+export default new ClienteSql()
