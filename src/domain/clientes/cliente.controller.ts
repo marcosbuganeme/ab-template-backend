21f80ab164a7c41ad9c877902827d61629d7181e
Marcos Olavo Silva Buganeme <marcos.after@gmail.com>
2020-09-13 19:12:49 -0300

    exportação de clientes e listagem de clientes exportados

--- src/domain/clientes/cliente.controller.ts
+++ src/domain/clientes/cliente.controller.ts
@@ -1,8 +1,47 @@
 import { Request, Response } from 'express';
 
-import { Cliente, ClienteService } from '.';
+import { ClienteService } from '.'
 
 class ClienteController {
+
+  async obterClientesExportados(req: Request, res: Response) {
+    try {
+
+      const { status, message, result } = await ClienteService.obterClientesExportados()
+
+      return res
+        .status(status)
+        .send({ message, result })
+
+    } catch (error) {
+      return res
+        .status(500)
+        .send({
+          error,
+          message: 'Falha no processo de exportação de dados da PCCLIENT'
+        });
+    }
+  }
+
+  async exportarDados(req: Request, res: Response) {
+    try {
+
+      await ClienteService.exportarDados()
+
+      return res
+        .status(200)
+        .send({ message: 'Dados da tabela PCCLIENT exportados.' })
+
+    } catch (error) {
+      return res
+        .status(500)
+        .send({
+          error,
+          message: 'Falha no processo de exportação de dados da PCCLIENT'
+        });
+    }
+  }
+
   async pesquisarCliente(req: Request, res: Response) {
     try {
       const { codigo, cliente } = req.query;
